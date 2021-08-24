74c8da9126edaeb9da7a2c8fa4355bae5e885e0a
Andrey Shtrauss <79809117+shtrausslearning@users.noreply.github.com>
2021-08-24 15:33:46 +0300

    Update max_i32.f90

--- max_i32.f90
+++ max_i32.f90
@@ -6,7 +6,9 @@ integer(kind = i32) :: my_int
 !The largest integer of this kind
 print*, huge(my_int)
 
-my_int = 1000000000 !  [2147483648,2147483647] i32
+!my_int = 10000000000  # too large
+my_int = 1000000000  # ok
+      ! [2147483648,2147483647] i32
 
 print*, my_int
 
