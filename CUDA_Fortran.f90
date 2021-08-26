b99a36be3f5c6fd80d78405489fd34a7c631ee47
Andrey Shtrauss <79809117+shtrausslearning@users.noreply.github.com>
2021-08-26 23:30:28 +0300

    Update CUDA_Fortran.f90

--- CUDA_Fortran.f90
+++ CUDA_Fortran.f90
@@ -1,3 +1,5 @@
+! SMALLER ARRAY CALCULATIONS
+
 module simpleOps_m
 contains
   subroutine inc(a,b)
@@ -50,13 +52,74 @@ allocate(a(n),a_d(n))
 a = 1
 b = 3
 
-a_d = a
-call inc <<<1,n>>>(a_d,b)
+a_d = a ! data transfer host -> device
+call inc <<<1,n>>>(a_d,b) ! single thread block
 a = a_d
 
 if(all(a==4)) then
     write(*,*) 'Test Passed'
 endif
 deallocate(a,a_d)
+
+end program incTest
+
+#####
+
+! F90
+module simpleOps_m
+contains
+  subroutine inc(a,b)
+  implicit none
+  integer :: a(:)
+  integer :: b
+  integer :: i,n
+  
+  n = size(a)
+  do i = 1,n
+    a(i) = a(i) + b
+  enddo
+  end subroutine inc
+end module simpleOps_m
+
+! CUDA F90
+module simpleOps_m
+contains
+  attributes(global) subroutine inc(a,b)
+  implicit none
+  integer :: a(:)
+  integer, value :: b
+  integer :: i
+  
+  i = threadIdx%x
+  a(i) = a(i) + b
+  
+  end subroutine inc
+end module simpleOps_m
+
+! LARGER ARRAY 
+
+! call <<<1,n>>>(a_d,b) -> limit 512/1024
+! larger arrays -> change the first execution parameter (<<<1,n>>>)
+
+program incTest
+use cudafor
+use simpleOps_m
+implicit none
+
+integer, parameter :: n = 1024*1024
+integer, parameter :: tPB = 256
+integer :: a(n), b
+integer, device :: a_d(n)
+
+a = 1
+b = 3
+
+a_d = a   ! multiple thread blocks: ceiling(real(n)/tPB)
+call inc <<<ceiling(real(n)/tPB),tPB>>>(a_d,b)
+a = a_d
+
+if(all(a==4)) then
+    write(*,*) 'Test Passed'
+endif
 
 end program incTest
